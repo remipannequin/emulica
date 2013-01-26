5d69fb21e1988a3f293fc9b586b8f3f6bec884ce
Rémi Pannequin <remi.pannequin@gmail.com>
2013-01-26 11:54:15 +0100

    add .emu extension if no extension have been filed (fixes #1105027)

--- src/emulica/emulicapp/__init__.py
+++ src/emulica/emulicapp/__init__.py
@@ -319,18 +319,18 @@ class Emulica:
                                         gtk.FILE_CHOOSER_ACTION_SAVE,
                                         (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
                                          gtk.STOCK_SAVE, gtk.RESPONSE_OK))
+	chooser.set_do_overwrite_confirmation(True)
         filter_emulica = gtk.FileFilter()
         filter_emulica.set_name("Emulica files")
         filter_emulica.add_pattern("*.emu")
-        filter_all = gtk.FileFilter()
-        filter_all.set_name("All files")
         chooser.add_filter(filter_emulica)
-        chooser.add_filter(filter_all)
         response = chooser.run()
         if response == gtk.RESPONSE_OK: 
             filename = chooser.get_filename()
         chooser.destroy()
-        
+	extension = os.path.splitext(filename)[1]
+        if not extension:
+	    filename = filename + '.emu'
         return filename
     
     def update_undo_redo_menuitem(self, *args):
