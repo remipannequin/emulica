f7e3e11dece3732f8cb2bf537614d38108b6a89a
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2019-03-07 09:51:52 +0100

    test for unassembly actuators

--- tests/test_sim10.py
+++ tests/test_sim10.py
@@ -21,70 +21,40 @@ In this test model, we use assemble actuators...
 two type of products are created, and then assembled, and finally put in inventory
 """
 
-import logging
-from emulica.core import set_up_logging
-set_up_logging(logging.ERROR)
+
 
 import unittest
 
 import util
 util.set_path()
 
+import logging
+from emulica.core import set_up_logging
+set_up_logging(logging.WARNING)
+
 from emulica.core import emulation
 import logging
 logger = logging.getLogger('test_sim10')
 
-EXP_RESULT_RESOURCE = [[(0, 0, 'setup'), 
-                        (0, 2, 'load'), 
-                        (7, 7, 'setup'), 
-                        (7, 8, 'unload'), 
-                        (8, 8, 'setup'), 
-                        (8, 10, 'load'), 
-                        (15, 15, 'setup'), 
-                        (15, 16, 'unload'), 
-                        (16, 16, 'setup'), 
-                        (16, 18, 'load'), 
-                        (23, 23, 'setup'), 
-                        (23, 24, 'unload'), 
-                        (24, 24, 'setup'), 
-                        (24, 26, 'load'), 
-                        (31, 31, 'setup'), 
-                        (31, 32, 'unload'), 
-                        (32, 32, 'setup'), 
-                        (32, 34, 'load'), 
-                        (39, 39, 'setup'), 
-                        (39, 40, 'unload'), 
-                        (40, 40, 'setup'), 
-                        (40, 42, 'load'), 
-                        (47, 47, 'setup'), 
-                        (47, 48, 'unload'), 
-                        (48, 48, 'setup'), 
-                        (48, 50, 'load'), 
-                        (55, 55, 'setup'), 
-                        (55, 56, 'unload')], 
-                       [(2, 2, 'setup'),
-                        (2, 7, 'p'),
-                        (10, 15, 'p'), 
-                        (18, 23, 'p'),
-                        (26, 31, 'p'),
-                        (34, 39, 'p'),
-                        (42, 47, 'p'),
-                        (50, 55, 'p')]]
+EXP_RESULT_RESOURCE = [[(5, 5, 'setup'), (5, 10.0, 'p'), (13.0, 18.0, 'p'), (21.0, 26.0, 'p'), (29.0, 34.0, 'p'), (37.0, 42.0, 'p'), (45.0, 50.0, 'p'), (53.0, 58.0, 'p')], 
+                       [(15.0, 15.0, 'setup'), (15.0, 20.0, 'p'), (24.0, 29.0, 'p'), (32.0, 37.0, 'p'), (40.0, 45.0, 'p'), (48.0, 53.0, 'p'), (56.0, 61.0, 'p'), (64.0, 69.0, 'p')]]
 
-EXP_RESULT_PRODUCT = [(1, [], [(0, 'source1'), (0, 'trans'), (2, 'assy_space'), (7, 'trans'), (8, 'sink')], 0, 100), 
-                      (2, [], [(0, 'source1'), (8, 'trans'), (10, 'assy_space'), (15, 'trans'), (16, 'sink')], 0, 100), 
-                      (3, [], [(0, 'source1'), (16, 'trans'), (18, 'assy_space'), (23, 'trans'), (24, 'sink')], 0, 100), 
-                      (4, [], [(0, 'source1'), (24, 'trans'), (26, 'assy_space'), (31, 'trans'), (32, 'sink')], 0, 100), 
-                      (5, [], [(0, 'source1'), (32, 'trans'), (34, 'assy_space'), (39, 'trans'), (40, 'sink')], 0, 100), 
-                      (6, [], [(0, 'source1'), (40, 'trans'), (42, 'assy_space'), (47, 'trans'), (48, 'sink')], 0, 100), 
-                      (7, [], [(0, 'source1'), (48, 'trans'), (50, 'assy_space'), (55, 'trans'), (56, 'sink')], 0, 100), 
-                      (8, [], [(0, 'source2'), (2, 'assy_space'), (7, 'trans'), (8, 'sink')], 0, 100), 
-                      (9, [], [(0, 'source2'), (10, 'assy_space'), (15, 'trans'), (16, 'sink')], 0, 100), 
-                      (10, [], [(0, 'source2'), (18, 'assy_space'), (23, 'trans'), (24, 'sink')], 0, 100), 
-                      (11, [], [(0, 'source2'), (26, 'assy_space'), (31, 'trans'), (32, 'sink')], 0, 100), 
-                      (12, [], [(0, 'source2'), (34, 'assy_space'), (39, 'trans'), (40, 'sink')], 0, 100), 
-                      (13, [], [(0, 'source2'), (42, 'assy_space'), (47, 'trans'), (48, 'sink')], 0, 100), 
-                      (14, [], [(0, 'source2'), (50, 'assy_space'), (55, 'trans'), (56, 'sink')], 0, 100)]
+
+EXP_RESULT_PRODUCT = [
+(1, [], [(0, 'source1'), (0, 'trans'), (2.0, 'assy_space'), (10.0, 'trans'), (11.0, 'buffer'), (13.0, 'trans'), (15.0, 'unassy_space'), (21.0, 'trans'), (22.0, 'out1')], 0, 100.0), 
+(2, [], [(1, 'source1'), (11.0, 'trans'), (13.0, 'assy_space'), (18.0, 'trans'), (19.0, 'buffer'), (22.0, 'trans'), (24.0, 'unassy_space'), (29.0, 'trans'), (30.0, 'out1')], 1, 100.0), 
+(3, [], [(3, 'source1'), (19.0, 'trans'), (21.0, 'assy_space'), (26.0, 'trans'), (27.0, 'buffer'), (30.0, 'trans'), (32.0, 'unassy_space'), (37.0, 'trans'), (38.0, 'out1')], 3, 100.0), 
+(4, [], [(5, 'source2'), (5, 'assy_space'), (10.0, 'trans'), (11.0, 'buffer'), (13.0, 'trans'), (15.0, 'unassy_space'), (20.0, 'out2')], 5, 100.0), 
+(5, [], [(6, 'source2'), (13.0, 'assy_space'), (18.0, 'trans'), (19.0, 'buffer'), (22.0, 'trans'), (24.0, 'unassy_space'), (29.0, 'out2')], 6, 100.0), 
+(6, [], [(7, 'source1'), (27.0, 'trans'), (29.0, 'assy_space'), (34.0, 'trans'), (35.0, 'buffer'), (38.0, 'trans'), (40.0, 'unassy_space'), (45.0, 'trans'), (46.0, 'out1')], 7, 100.0), 
+(7, [], [(7, 'source2'), (21.0, 'assy_space'), (26.0, 'trans'), (27.0, 'buffer'), (30.0, 'trans'), (32.0, 'unassy_space'), (37.0, 'out2')], 7, 100.0), 
+(8, [], [(9, 'source2'), (29.0, 'assy_space'), (34.0, 'trans'), (35.0, 'buffer'), (38.0, 'trans'), (40.0, 'unassy_space'), (45.0, 'out2')], 9, 100.0), 
+(9, [], [(11, 'source2'), (37.0, 'assy_space'), (42.0, 'trans'), (43.0, 'buffer'), (46.0, 'trans'), (48.0, 'unassy_space'), (53.0, 'out2')], 11, 100.0), 
+(10, [], [(12, 'source1'), (35.0, 'trans'), (37.0, 'assy_space'), (42.0, 'trans'), (43.0, 'buffer'), (46.0, 'trans'), (48.0, 'unassy_space'), (53.0, 'trans'), (54.0, 'out1')], 12, 100.0), 
+(11, [], [(20, 'source1'), (43.0, 'trans'), (45.0, 'assy_space'), (50.0, 'trans'), (51.0, 'buffer'), (54.0, 'trans'), (56.0, 'unassy_space'), (61.0, 'trans'), (62.0, 'out1')], 20, 100.0), 
+(12, [], [(23, 'source2'), (45.0, 'assy_space'), (50.0, 'trans'), (51.0, 'buffer'), (54.0, 'trans'), (56.0, 'unassy_space'), (61.0, 'out2')], 23, 100.0), 
+(13, [], [(30, 'source1'), (51.0, 'trans'), (53.0, 'assy_space'), (58.0, 'trans'), (59.0, 'buffer'), (62.0, 'trans'), (64.0, 'unassy_space'), (69.0, 'trans'), (70.0, 'out1')], 30, 100.0), 
+(14, [], [(35, 'source2'), (53.0, 'assy_space'), (58.0, 'trans'), (59.0, 'buffer'), (62.0, 'trans'), (64.0, 'unassy_space'), (69.0, 'out2')], 35, 100.0)]
 
 EMULATE_UNTIL = 100;
 
@@ -96,17 +66,14 @@ class ControlCreate:
         rp_crea1 = create1.create_report_socket()
         rp_crea2 = create2.create_report_socket()
         dates1 = [0, 1, 3, 7, 12, 20, 30]
-        requests1 = [emulation.Request("create1", "create",params={'productType':'type1', 'date': d}) for d in dates1]
+        requests1 = [emulation.Request("create1", "create",params={'productType':'type1'}, date=d) for d in dates1]
         dates2 = [5, 6, 7, 9, 11, 23, 35]
-        requests2 = [emulation.Request("create2", "create",params={'productType':'type1', 'date': d}) for d in dates2]
+        requests2 = [emulation.Request("create2", "create",params={'productType':'type2'}, date=d) for d in dates2]
         for rq in requests1:
             yield create1.request_socket.put(rq)
-        for i in range(7):
-            yield rp_crea1.get()
         for rq in requests2:
             yield create2.request_socket.put(rq)
-        for i in range(7):
-            yield rp_crea2.get()
+
 
 class ControlAssy:
     def run(self, model):
@@ -126,15 +93,13 @@ class ControlAssy:
             logger.info("pce 1 prete")
             
             logger.info("chargement")
-            rq = emulation.Request("trans","move",params={'program':'load'})
+            rq = emulation.Request("trans","move",params={'program':'load_assy'})
             yield trans.request_socket.put(rq)
             ##pièces prêtes
             yield rp_obs2.get()
             logger.info("pce 2 prete")
             yield rp_obs_assy.get()
             logger.info("pce assy chargée")
-            #print self.got[0]
-            #yield emulation.put, self, assy.request_socket, [emulation.Request("assy","setup", params={"program":'p'})]
             ##début process
             logger.info("process")
             yield assy.request_socket.put(emulation.Request("assy","assy", params={"program":'p'}))
@@ -142,11 +107,40 @@ class ControlAssy:
             fin = False
             while not fin:
                 ev = yield rp_assy.get()
-                logger.info(ev[0])
-                fin = ev[0].what=="idle"
+                logger.info(ev)
+                fin = ev.what=="idle"
             ##déchargement
             logger.info("dechargement")
-            yield trans.request_socket.put(emulation.Request("trans", "move", params={"program": 'unload'}))
+            yield trans.request_socket.put(emulation.Request("trans", "move", params={"program": 'unload_assy'}))
+
+
+class ControlUnassy:
+    def run(self, model):
+        obs = model.modules["obs_buffer"]
+        rp_obs = obs.create_report_socket()
+        obs_unassy = model.modules["obs_unassy"]
+        rp_obs_unassy = obs_unassy.create_report_socket()
+        trans = model.modules["trans"]
+        unassy = model.modules["unassy"]
+        rp_unassy = unassy.create_report_socket()
+        while True:
+            ev = yield rp_obs.get()
+            rq = emulation.Request("trans","move",params={'program':'load_unassy'})
+            yield trans.request_socket.put(rq)
+            # wait for product to be loaded
+            yield rp_obs_unassy.get()
+            ##début process
+            logger.info("unassy process")
+            yield unassy.request_socket.put(emulation.Request("unassy","unassy", params={"program":'p'}))
+            ##attente fin process
+            fin = False
+            while not fin:
+                ev = yield rp_unassy.get()
+                fin = ev.what=="idle"
+            rq = emulation.Request("trans","move",params={'program':'unload_unassy'})
+            yield trans.request_socket.put(rq)
+            
+
 
 def get_model():
     model = emulation.Model()
@@ -157,16 +151,25 @@ def get_model():
     create1 = emulation.CreateAct(model, "create1", destination = source1)
     create2 = emulation.CreateAct(model, "create2", destination = source2)
     assy_space = emulation.Holder(model, "assy_space")
+    unassy_space = emulation.Holder(model, "unassy_space")
     obs_assy = emulation.PushObserver(model, "obs_assy", holder = assy_space)
     assy = emulation.AssembleAct(model, "assy", assy_holder = assy_space)
+    unassy = emulation.DisassembleAct(model, "unassy", unassy_holder = unassy_space)
+    obs_unassy = emulation.PushObserver(model, "obs_unassy", holder = unassy_space)
     trans = emulation.SpaceAct(model, "trans")
-    sink = emulation.Holder(model, "sink")
-    obs_sink = emulation.PushObserver(model, "obs_sink", holder = sink)
-    trans.add_program('load', 2, {'source':source1, 'destination':assy_space})
-    trans.add_program('unload', 1, {'source':assy_space, 'destination':sink})
+    buff = emulation.Holder(model, "buffer")
+    out1 = emulation.Holder(model, "out1")
+    out2 = emulation.Holder(model, "out2")
+    obs_buff = emulation.PushObserver(model, "obs_buffer", holder = buff)
+    trans.add_program('load_assy', 2, {'source':source1, 'destination':assy_space})
+    trans.add_program('unload_assy', 1, {'source':assy_space, 'destination':buff})
+    trans.add_program('load_unassy', 2, {'source':buff, 'destination':unassy_space})
+    trans.add_program('unload_unassy', 1, {'source':unassy_space, 'destination':out1})
     assy.add_program('p', 5, {'source':source2})
+    unassy.add_program('p', 5, {'destination':out2})
     model.register_control(ControlCreate)
     model.register_control(ControlAssy)
+    model.register_control(ControlUnassy)
     return model
 
 
@@ -189,7 +192,8 @@ class TestSim10(unittest.TestCase):
                        p.space_history, 
                        p.create_time, 
                        p.dispose_time) for (pid, p) in model.products.items()]
-        result_resource = [model.modules["trans"].trace, model.modules["assy"].trace]
+        result_resource = [model.modules["assy"].trace, model.modules["unassy"].trace]
+        #print(result_resource)
         self.assertEqual(result_product, EXP_RESULT_PRODUCT)
         self.assertEqual(result_resource, EXP_RESULT_RESOURCE)
 
